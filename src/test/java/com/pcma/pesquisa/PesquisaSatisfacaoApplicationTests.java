7f645baecdd5bbb158aead96bb69efbac3a6d8c5
David Gomesh <davidgomesh2016@gmail.com>
2022-03-11 14:11:07 -0300

    Adicionado Spring

---
+++ src/test/java/com/pcma/pesquisa/PesquisaSatisfacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcma.pesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PesquisaSatisfacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
