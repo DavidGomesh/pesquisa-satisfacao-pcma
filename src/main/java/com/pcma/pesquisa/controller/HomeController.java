350a3ad86922e6bb2962413d24c4c8893bc13d04
David Gomesh <davidgomesh2016@gmail.com>
2022-03-14 08:29:50 -0300

    Adicionado TAG form

--- src/main/java/com/pcma/pesquisa/controller/HomeController.java
+++ src/main/java/com/pcma/pesquisa/controller/HomeController.java
@@ -2,11 +2,17 @@ package com.pcma.pesquisa.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
     public String home(){
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String salvarAvaliacao(){
         return "home";
     }
 }
