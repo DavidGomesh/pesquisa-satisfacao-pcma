bf477a288c808968199b5bb610aab102cef04bc0
David Gomesh <davidgomesh2016@gmail.com>
2022-03-16 10:37:15 -0300

    Adicionado cores na avaliacao

--- src/main/resources/static/assets/js/setAvaliacao.js
+++ src/main/resources/static/assets/js/setAvaliacao.js
@@ -1,32 +1,44 @@
 
 // Scrip para controlar a mudança de opção na Avaliação
 let idUtimaAvaliacao = null;
+let classUltimaAvaliacao = "";
 
-function setAvalicao(avalicao) {
+function setAvaliacao(avaliacao) {
     let idAvaliacao = "";
+
+    let rowAvaliacaoAtual;
     let lblAvaliacaoAtual;
+
+    let rowUltimaAvaliacao;
     let lblUltimaAvaliacao;
 
-    switch (avalicao) {
-        case 1: idAvaliacao = "icon-avalicao-otimo"; break;
-        case 2: idAvaliacao = "icon-avalicao-bom"; break;
-        case 3: idAvaliacao = "icon-avalicao-regular"; break;
-        case 4: idAvaliacao = "icon-avalicao-ruim"; break;
-        case 5: idAvaliacao = "icon-avalicao-pessimo"; break;
+    switch (avaliacao) {
+        case 1: idAvaliacao = "avaliacao-pessimo"; break;
+        case 2: idAvaliacao = "avaliacao-ruim"; break;
+        case 3: idAvaliacao = "avaliacao-regular"; break;
+        case 4: idAvaliacao = "avaliacao-bom"; break;
+        case 5: idAvaliacao = "avaliacao-otimo"; break;
     }
 
-    // Deixa a opcao selecionada em negrito
-    lblAvaliacaoAtual = document.querySelector(`#${idAvaliacao}`);
+    // Deixa a opcao selecionada em negrito e muda a cor
+    rowAvaliacaoAtual = document.querySelector(`#row-${idAvaliacao}`);
+    rowAvaliacaoAtual.classList.add(`avaliacao-${avaliacao}`);
+    
+    lblAvaliacaoAtual = document.querySelector(`#icon-${idAvaliacao}`);
     lblAvaliacaoAtual.classList.remove("far");
     lblAvaliacaoAtual.classList.add("fas");
-
-    // Tira o negrito da ultima opcao selecionada
+    
+    // Tira o negrito e a cor da ultima opcao selecionada
     if (idUtimaAvaliacao != null && idUtimaAvaliacao != idAvaliacao) {
-        lblUltimaAvaliacao = document.querySelector(`#${idUtimaAvaliacao}`);
+        rowUltimaAvaliacao = document.querySelector(`#row-${idUtimaAvaliacao}`);
+        rowUltimaAvaliacao.classList.remove(classUltimaAvaliacao);
+        
+        lblUltimaAvaliacao = document.querySelector(`#icon-${idUtimaAvaliacao}`);
         lblUltimaAvaliacao.classList.remove("fas");
         lblUltimaAvaliacao.classList.add("far");
     }
-
-    // Define a avaliacao atual como a ultima selecionada
+    
+    // Define a avaliacao atual como a ultima selecionada e guarda a class tbm
+    classUltimaAvaliacao = `avaliacao-${avaliacao}`;
     idUtimaAvaliacao = idAvaliacao;
 }
