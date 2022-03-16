966c8167fca34463522ddfc36841e55be16f5753
David Gomesh <davidgomesh2016@gmail.com>
2022-03-16 14:35:47 -0300

    Reorganizado os scripts da página de pesquisa

--- src/main/resources/static/assets/js/validarCpf.js
+++ src/main/resources/static/assets/js/validarCpf.js
@@ -1,37 +1,39 @@
-function validarCPF(cpf) {	
-	cpf = cpf.replace(/[^\d]+/g,'');	
-	if(cpf == '') return false;	
-	// Elimina CPFs invalidos conhecidos	
-	if (cpf.length != 11 || 
-		cpf == "00000000000" || 
-		cpf == "11111111111" || 
-		cpf == "22222222222" || 
-		cpf == "33333333333" || 
-		cpf == "44444444444" || 
-		cpf == "55555555555" || 
-		cpf == "66666666666" || 
-		cpf == "77777777777" || 
-		cpf == "88888888888" || 
-		cpf == "99999999999")
-			return false;		
-	// Valida 1o digito	
-	add = 0;	
-	for (i=0; i < 9; i ++)		
-		add += parseInt(cpf.charAt(i)) * (10 - i);	
-		rev = 11 - (add % 11);	
-		if (rev == 10 || rev == 11)		
-			rev = 0;	
-		if (rev != parseInt(cpf.charAt(9)))		
-			return false;		
-	// Valida 2o digito	
-	add = 0;	
-	for (i = 0; i < 10; i ++)		
-		add += parseInt(cpf.charAt(i)) * (11 - i);	
-	rev = 11 - (add % 11);	
-	if (rev == 10 || rev == 11)	
-		rev = 0;	
-	if (rev != parseInt(cpf.charAt(10)))
-		return false;	
-		alert("ok")
-	return true;   
+function validarCPF(strCPF) {
+	// alert("Saçve")
+	var x = strCPF.replace('.', '');
+	var x = x.replace('.', '');
+	var strCPF = x.replace('-', '');
+	var Soma = 0;
+	var Resto;
+	if (strCPF == "00000000000" || strCPF == "11111111111" || strCPF == "22222222222" ||
+		strCPF == "33333333333" || strCPF == "44444444444" || strCPF == "55555555555" ||
+		strCPF == "66666666666" || strCPF == "77777777777" || strCPF == "88888888888" ||
+		strCPF == "99999999999")
+		return false;
+
+	for (i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+	Resto = (Soma * 10) % 11;
+
+	if ((Resto == 10) || (Resto == 11)) Resto = 0;
+	if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+
+	Soma = 0;
+	for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+	Resto = (Soma * 10) % 11;
+
+	if ((Resto == 10) || (Resto == 11)) Resto = 0;
+	if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+	return true;
 }
+
+$("#cpf").keyup(function (e) {
+	var input = $('#cpf').val();
+	var x = validarCPF(input);
+	if (!x) {
+		$("#cpf").removeClass("is-valid");
+		$("#cpf").addClass("is-invalid");
+	} else {
+		$("#cpf").removeClass("is-invalid");
+		$("#cpf").addClass("is-valid");
+	}
+});
